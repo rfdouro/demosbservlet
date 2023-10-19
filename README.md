2098653c753a6bdad75f0f07f6a77444e4943434
Rômulo F. Douro <rfdouro@gmail.com>
2023-10-19 09:49:25 -0300

    deploy

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# demosbservlet
+Projeto de demonstração de uso de Spring boot com Servlets e JSP
---
+++ src/main/java/br/org/rfdouro/demosbservlet/DemosbservletApplication.java
@@ -0,0 +1,13 @@
+package br.org.rfdouro.demosbservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosbservletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosbservletApplication.class, args);
+	}
+
+}
